1a29dc22fe14e4375b3906e2c191ce4e29dd26c3
Khanh Doan <khanhdd@neo.vn>
2022-03-17 14:19:28 +0700

    initial commit

---
+++ component/App.js
@@ -0,0 +1,17 @@
+import html from '../core.js'
+
+import Header from '../component/Header.js'
+import Footer from '../component/Footer.js'
+import TodoList from '../component/TodoList.js'
+
+function App(){
+    return html`
+        <section class="todoapp">
+            ${Header()}
+            ${TodoList()}
+            ${Footer()}
+        </section>
+    `
+}
+
+export default App
